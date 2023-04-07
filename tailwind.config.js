31313263a0a39eb3e5c16395ad49a43de31dad64
OmPTEL <118986497+OmPTEL@users.noreply.github.com>
2023-04-07 15:17:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+      mullish:["Mulish","sans-serif"],
+      fraunces:["Fraunces", "serif"],
+      italics:["Brygada 1918", "serif"],
+      raleway:["Ralewa", "sans-serif"],
+      rubika:["Rubik","sans-serif"],
+      philo:["Philosopher", "sans-serif"]
+    },
+  colors:{
+    deepBlue:"#02042a",
+    lightBlue:"#2b84ea",
+    lightBlue300:"#4b94ed",
+    greenLight:"#61cea6",
+    grayText:"#818597",
+    lightGray:"#344a6c",
+    deepBlueHead:"#162f56",
+    gray2:"#525a76",
+    Blue:"#0066ff",
+    darbB:"#003399",
+    db:"#004d99",
+  },},
+  },
+  plugins: [],
+}
